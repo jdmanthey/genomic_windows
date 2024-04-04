16ea4517454fe421b874debd7a0603627a989c34
Joe Manthey <jdmanthey@gmail.com>
2024-04-04 16:20:49 -0500

    Update window_stat_calculations.r

--- window_stat_calculations.r
+++ window_stat_calculations.r
@@ -72,22 +72,24 @@ fis <- function(xxx, popmap, outname, filename) {
     # select all individuals in the same population
     a_pop <- xxx[,4:ncol(xxx)]
     a_pop <- a_pop[ ,popmap$PopName == popmap$PopName[a]]
-    # calculate expected heterozygosity for each site
-    a_eh <- apply(a_pop, 1, expected_heterozygosity)
-    # average expected heterozygosity across all sites
-    a_eh <- mean(a_eh)
-    # calculate observed heterozygosity for this individual
-    # remove missing genotypes
-    a_rep <- a_rep[a_rep != "./."]
-    # remove phasing information
-    a_rep <- gsub("\\|", "/", a_rep)
-    # find observed heterozygosity
-    a_het <- length(a_rep[a_rep == "0/1"]) / length(a_rep)
-    # compare expected and observed heterozygosities to calculate Fis
-    fis <- 1 - (a_eh - a_het) / a_eh
-    # add to output
-    output_rep <- c(output_rep, length(a_rep), length(a_rep[a_rep == "0/1"]), fis)
-    write(output_rep, file=outname, append=T, ncolumns=9, sep="\t")
+    if(is.null(nrow(a_pop)) == FALSE) {
+    	    # calculate expected heterozygosity for each site
+    	a_eh <- apply(a_pop, 1, expected_heterozygosity)
+    	# average expected heterozygosity across all sites
+    	a_eh <- mean(a_eh)
+    	# calculate observed heterozygosity for this individual
+    	# remove missing genotypes
+    	a_rep <- a_rep[a_rep != "./."]
+    	# remove phasing information
+    	a_rep <- gsub("\\|", "/", a_rep)
+    	# find observed heterozygosity
+    	a_het <- length(a_rep[a_rep == "0/1"]) / length(a_rep)
+    	# compare expected and observed heterozygosities to calculate Fis
+    	fis <- 1 - (a_eh - a_het) / a_eh
+    	# add to output
+    	output_rep <- c(output_rep, length(a_rep), length(a_rep[a_rep == "0/1"]), fis)
+    	write(output_rep, file=outname, append=T, ncolumns=9, sep="\t")
+    }
   }
 }
 
@@ -397,10 +399,9 @@ titv <- function(xxx, popmap, outname, filename) {
   titv_ratio <- total_transitions / (a_total - total_transitions)
   
   # output info
-  output_rep <- c("all_inds", "none", "titv", strsplit(filename, ":")[[1]][1], 
-                  as.numeric(strsplit(strsplit(filename, ":")[[1]][2], "-")[[1]][1]),
-                  as.numeric(strsplit(strsplit(filename, "-")[[1]][2], ".simple")[[1]][1]))
-  
+  output_rep <- c("all_inds", "none", "titv", strsplit(filename, "__")[[1]][1], 
+                   as.numeric(strsplit(strsplit(filename, "__")[[1]][2], "__")[[1]][1]),
+                   as.numeric(strsplit(strsplit(filename, "__")[[1]][3], ".simple")[[1]][1]))
   output_rep <- output_rep <- c(output_rep, a_total, a_het, titv_ratio)
   write(output_rep, file=outname, append=T, ncolumns=9, sep="\t")
   
@@ -438,16 +439,16 @@ create_fasta_from_vcf <- function(xxx, popmap, outname, filename, num_sites_need
         genotypes[a,] <- gsub("\\./\\.", "?", genotypes[a,])
       } else { # if polymorphic
         both_genotypes <- sort(as.character(allele_info[a,]))
-        if(both_genotypes[1] == "A" & both_genotypes[2] == "C") { het = "M" }
-        if(both_genotypes[1] == "A" & both_genotypes[2] == "G") { het = "R" }
-        if(both_genotypes[1] == "A" & both_genotypes[2] == "T") { het = "W" }
-        if(both_genotypes[1] == "C" & both_genotypes[2] == "G") { het = "S" }
-        if(both_genotypes[1] == "C" & both_genotypes[2] == "T") { het = "Y" }
-        if(both_genotypes[1] == "G" & both_genotypes[2] == "T") { het = "K" }
+        if(both_genotypes[1] == "A" & both_genotypes[2] == "C") { het = c("A", "C") }
+        if(both_genotypes[1] == "A" & both_genotypes[2] == "G") { het = c("A", "G") }
+        if(both_genotypes[1] == "A" & both_genotypes[2] == "T") { het = c("A", "T") }
+        if(both_genotypes[1] == "C" & both_genotypes[2] == "G") { het = c("C", "G") }
+        if(both_genotypes[1] == "C" & both_genotypes[2] == "T") { het = c("C", "T") }
+        if(both_genotypes[1] == "G" & both_genotypes[2] == "T") { het = c("G", "T") }
         genotypes[a,] <- gsub("0/0", allele_info[a,1], genotypes[a,])
         genotypes[a,] <- gsub("\\./\\.", "?", genotypes[a,])
         genotypes[a,] <- gsub("1/1", allele_info[a,2], genotypes[a,])
-        genotypes[a,] <- gsub("0/1", het, genotypes[a,])
+        genotypes[a,][genotypes[a,] == "0/1"] <- sample(het, length(genotypes[a,][genotypes[a,] == "0/1"]), replace=T)
       }
     }
     
@@ -463,7 +464,6 @@ create_fasta_from_vcf <- function(xxx, popmap, outname, filename, num_sites_need
     }
   }
 }
-
 
 
 
